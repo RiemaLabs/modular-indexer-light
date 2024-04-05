5b85556d95069ffaf6bc26f61938ba900e4dc3ab
yaoge <addremote@163.com>
2024-04-05 20:38:49 +0800

    chore: run.bat

--- run.bat
+++ run.bat
@@ -1,53 +1,48 @@
-@echo off  
-setlocal enabledelayedexpansion  
-  
-set "execFile=light-indexer.exe"  
-set "configExampleFile=config.example.json"  
-set "configFile=config.json"  
-set "bitcoinRPC=https://bitcoin-mainnet-archive.allthatnode.com"  
-set /a "randName=!RANDOM!%RANDOM%!RANDOM!%RANDOM%!RANDOM!%RANDOM!"  
-randName=!randName:~0,6!  
-  
-if not exist "%configExampleFile%" (  
-    echo %configExampleFile% not found  
-    exit /b 1  
-)  
-  
-copy "%configExampleFile%" "%configFile%" >nul  
-  
-if not exist "%execFile%" (  
-    echo %execFile% not found  
-    exit /b 1  
-)  
-  
-set /p "endpoint=Please enter a Bitcoin RPC: "  
-if not "!endpoint!"=="" (  
-    echo !endpoint! | findstr /r /c:"^https?://[^/\s]+/?[^/\s]*\.?[^/\s]*$" >nul  
-    if !errorlevel! eq 0 (  
-        powershell -Command "(Get-Content '%configFile%').replace('%bitcoinRPC%', '!endpoint!') | Set-Content '%configFile%'"  
-    ) else (  
-        echo Invalid Bitcoin RPC, will use default Bitcoin RPC %bitcoinRPC%  
-    )  
-)  
-  
-set /p "report=Would you like to upload verified checkpoint to DA? [yes/no] "  
-if /i "!report!"=="yes" (  
-    set /p "gasCoupon=Please enter a Gas Coupon: "  
-    if "!gasCoupon!"=="" (  
-        echo Gas coupon is needed  
-        exit /b 1  
-    )  
-    powershell -Command "(Get-Content '%configFile%').replace('YourGasCoupon', '!gasCoupon!') | Set-Content '%configFile%'"  
-  
-    set /p "name=Please enter indexer name: "  
-    if "!name!"=="" (  
-        set "name=!randName!"  
-    )  
-    powershell -Command "(Get-Content '%configFile%').replace('YourOwnLightIndexerName', '!name!') | Set-Content '%configFile%'"  
-) else (  
-    set "command=!execFile! --report=false"  
-)  
-  
-echo Starting indexer...  
-!command!  
-endlocal
+@echo off
+setlocal enabledelayedexpansion
+
+set execFile=light-indexer.exe
+set configFile=config.json
+set configExampleFile=config.example.json
+set bitcoinRPC=https://bitcoin-mainnet-archive.allthatnode.com
+
+if not exist "%configExampleFile%" (
+    echo %configExampleFile% not found
+    exit /b 1
+)
+copy /y %configExampleFile% %configFile%
+
+if not exist "%execFile%" (
+    echo %execFile% not found
+    exit /b 1
+)
+
+set /p endpoint=please enter a bitcoin rpc: 
+echo %endpoint% | findstr /r /c:"^http[s]*:\/\/.*" > nul
+if %errorlevel% neq 0 (
+    echo invalid bitcoin rpc, will use default bitcoin rpc %bitcoinRPC%
+    set endpoint=%bitcoinRPC%
+)
+powershell -Command "(gc %configFile%) -replace '%bitcoinRPC%', '%endpoint%' | Out-File -encoding ASCII %configFile%"
+
+set /p report=would you like upload verified checkpoint to DA ? [yes/no] 
+if /i "%report%"=="yes" (
+    set /p gasCoupon=please enter a Gas Coupon: 
+    if "!gasCoupon!"=="" (
+        echo gas coupon is needed
+        exit /b 1
+    )
+    powershell -Command "(gc %configFile%) -replace 'YourGasCoupon', '!gasCoupon!' | Out-File -encoding ASCII %configFile%"
+
+    set /p name=please enter indexer name: 
+    if "!name!"=="" (
+        echo use randomly generated name
+        set /a name=%RANDOM%
+    )
+    powershell -Command "(gc %configFile%) -replace 'YourOwnLightIndexerName', '!name!' | Out-File -encoding ASCII %configFile%"
+) else (
+    set command=%execFile% --report=false
+)
+
+echo start indexer....
+%command%
--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-set -x
+# set -x
 
 execFile="light-indexer"
 command="./$execFile"
