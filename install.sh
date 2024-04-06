14e64fe4a3c03f0794c3d6a8de04172a476a895f
yaoge <addremote@163.com>
2024-04-06 17:30:10 +0800

    replace wget with curl

--- install.sh
+++ install.sh
@@ -16,7 +16,7 @@ if uname -r | grep -qEi "(Microsoft|WSL)"; then
 fi
 
 commandExist "unzip"
-commandExist "wget"
+commandExist "curl"
 
 system=$(uname -s)
 system=$(echo "$system" | tr '[:upper:]' '[:lower:]')
@@ -27,12 +27,12 @@ if [[ $arch == "x86_64" ]]; then
 fi
 
 # latest_release=$(curl -s "https://api.github.com/repos/RiemaLabs/modular-indexer-light/releases/latest")
-version=$(wget -qO- -t1 -T2 "https://api.github.com/repos/RiemaLabs/modular-indexer-light/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g')
+version=$(curl -s "https://api.github.com/repos/RiemaLabs/modular-indexer-light/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g')
 
 zipfile="light-indexer-$system-$arch.zip"
 
 download_url="https://github.com/RiemaLabs/modular-indexer-light/releases/download/$version/$zipfile"
-wget -t2 -T2 -c $download_url
+curl -O $download_url
 unzip $zipfile
 
 rm -f $zipfile
