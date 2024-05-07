da8471013d59cab1754371742d207e60a43e34c4
MeihaoZuyu <anqur@riema.xyz>
2024-05-07 20:55:08 +0800

    feat: (JS SDK `v0.2.0-rc.2`) Bundle all the assets into a single JS file
    Signed-off-by: MeihaoZuyu <anqur@riema.xyz>

--- packages/modular-indexer-light-sdk/index.ts
+++ packages/modular-indexer-light-sdk/index.ts
@@ -1,3 +1,7 @@
+import "./wasm_exec.js";
+// @ts-ignore
+import init from "./modular-indexer-light.wasm?init";
+
 declare const Go: any;
 
 /**
@@ -5,10 +9,7 @@ declare const Go: any;
  */
 export async function lightLoadWasm() {
     const go = new Go();
-    const response = await fetch('./modular-indexer-light.wasm');
-    const buffer = await response.arrayBuffer();
-    const result = await WebAssembly.instantiate(buffer, go.importObject);
-    go.run(result.instance);
+    go.run(await init(go.importObject));
 }
 
 interface Config {
---
+++ packages/modular-indexer-light-sdk/vite.config.mts
@@ -0,0 +1,16 @@
+import {resolve} from "path"
+import {defineConfig} from "vite"
+import dts from "vite-plugin-dts"
+
+export default defineConfig({
+    build: {
+        assetsInlineLimit: 1024 * 1024 * 1024,
+        lib: {
+            entry: resolve(__dirname, "index.ts"),
+            formats: ["es"]
+        },
+    },
+    plugins: [
+        dts()
+    ],
+})
